9ebeee7c966bc268d45d8d9ed9d3f6c2e4019ad8
Timothy Olili <timmolili27@gmail.com>
2024-10-18 08:01:07 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
-Person.prototype.greet = function() {
-	return `Hello, my name is ${this.name}, I am ${this.age} years old.`
-}
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
 function Employee(name, age, jobTitle) {
-	Person.call(this, name, age);
-	this.jobTitle = jobTitle;
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 
-Object.setPrototypeOf(Employee.prototype, Person.prototype)
+Object.setPrototypeOf(Employee.prototype, Person.prototype);
 
-Employee.prototype.jobGreet = function() {
-	return `Hello, my name is ${this.name}, I am ${this.age} years old and and my job title is ${this.jobTitle}.`
-}
+Employee.prototype.jobGreet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old and and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
